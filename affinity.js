ddd95fa438b0e3ef04330364f0930bec0146623f
Spoopypingas <arranzgomezjoseluis@gmail.com>
2024-05-01 20:16:40 +0200

    a la verga

--- affinity.js
+++ affinity.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const mysql = require('mysql');
 const ejs = require('ejs');
 const path = require('path');
@@ -11,6 +12,13 @@ const connection = require('./database.js');
 
 app.use(express.static(path.join(__dirname, 'public')));    // Donde voya a colocar mis ficheros de imagen, css, javascript
 
+// Sesión de express-session
+
+app.use(session({
+  secret: 'secretKey',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Configurar middleware para analizar el cuerpo de las solicitudes
 app.use(express.urlencoded({ extended: true }));
@@ -40,6 +48,36 @@ app.post('/signup', (req, res) => {
   });
 });
 
+app.post('/signup', (req, res) => {
+  // Verificar las credenciales del usuario
+  if (usuarioValido) {
+      // Crear una sesión de usuario
+      req.session.ID_usuario = usuario.ID_usuario;
+      req.session.Mail = usuario.Mail;
+      res.send('Iniciaste sesión correctamente');
+  } else {
+      res.send('Credenciales incorrectas');
+  }
+});
+
+
+
+
+
+app.post('/login', (req, res) => {
+  // Verificar las credenciales del usuario
+  if (usuarioValido) {
+      // Crear una sesión de usuario
+      req.session.usuarioId = usuario.id;
+      req.session.username = usuario.username;
+      res.send('Iniciaste sesión correctamente');
+  } else {
+      res.send('Credenciales incorrectas');
+  }
+});
+
+
+
 // Obtención de la página aficiones
 app.get('/aficiones', (req, res) => {
   res.sendFile(__dirname + '/aficiones.html');
@@ -48,14 +86,22 @@ app.get('/aficiones', (req, res) => {
 // Ruta de registro de aficiones
 
 app.post('/aficiones', (req, res) => {
+  // Obtener el ID del usuario de la sesión
+  const ID_usuario = req.session.ID_usuario; // Suponiendo que has guardado el ID del usuario en la sesión durante el inicio de sesión
+  
+  // Crear el objeto con los datos de la afición
   const aficioncuenta = {
-    ID_usuario: req.body.ID_usuario,
+    ID_usuario: ID_usuario, // Usar el ID del usuario de la sesión
     ID_aficion: req.body.ID_aficion,
   };
-  // Insertar nuevo usuario en la base de datos
+
+  // Insertar el registro en la base de datos
   connection.query('INSERT INTO usuario_aficion SET ?', aficioncuenta, (error, results) => {
-    if (error) throw error;
-    //res.send('Usuario registrado exitosamente');
+    if (error) {
+      console.error('Error al registrar la afición:', error);
+      res.status(500).send('Error interno del servidor al registrar la afición');
+      return;
+    }
     res.sendFile(__dirname + '/login.html');
   });
 });
