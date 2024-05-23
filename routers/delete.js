7db2f36bc48cc673a7243005438dfcce73509dc5
Spoopypingas <arranzgomezjoseluis@gmail.com>
2024-05-23 20:40:43 +0200

    Actualizar perfil y borrarlo

--- routers/delete.js
+++ routers/delete.js
@@ -96,4 +96,36 @@ eliminar.get('/users', (req, res)=> {
     });
   });
 
+
+
+  //Página para presentar datos para eliminar los usuarios
+eliminar.get('/borcuenta', (req, res)=> {
+  const parametro = url.parse(req.url, true);
+  const iduser = parametro.query.ID_usuario;
+  const query = 'SELECT * FROM usuarios WHERE ID_usuario= ?';
+  connection.query(query, [iduser], (error, results) => {
+    if (error) {
+      console.error('Error al obtener los registros de la tabla "usuarios": ', error);
+      res.status(500).send('Error del servidor');
+    } else {
+      res.render('updatePerfil', { perfil: results[0], userID: iduser });
+    }
+  });
+});
+
+// Página de eliminación de usuarios
+                                ////////////// CUIDADO ////////////////////
+/////////// Si todavía tengo registros en otras tablas dependientes de usuarios no se eliminarán ///////////////
+eliminar.post('/borcuenta', (req, res) => {
+  const {iduser} = req.body;
+  const query = 'DELETE FROM usuarios WHERE ID_usuario = ?';
+  connection.query(query, [iduser], (error, result) => {
+    if (error) {
+      console.error('Error al eliminar registro en la tabla "usuarios": ', error);
+      res.status(500).send('Error del servidor');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 module.exports = eliminar;
--- routers/update.js
+++ routers/update.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const actualizar = express.Router(); // Este objeto es que vamos a exportar
+const actualizar = express.Router(); // Este objeto es el que vamos a exportar
 const connection = require('../database.js');
 
 // Página para presentar datos para actualizar el perfil del usuario
 actualizar.get('/actdatos', (req, res) => {
-  const iduser = parametro.query.ID_usuario;
-  const query = 'SELECT * FROM usuarios WHERE `ID_usuario`="' + iduser + '"';
-  connection.query(query, [userID], (error, results) => {
+  const iduser = req.query.ID_usuario;
+  const query = 'SELECT * FROM usuarios WHERE ID_usuario = ?';
+  connection.query(query, [iduser], (error, results) => {
     if (error) {
       console.error('Error al obtener los registros de la tabla "usuarios": ', error);
       res.status(500).send('Error del servidor');
     } else {
-      res.render('updatePerfil', { perfil: results[0], userID: userID });
+      res.render('updatePerfil', { perfil: results[0], userID: iduser });
     }
   });
 });
 
 // Página de actualización del perfil del usuario
 actualizar.post('/actdatos', (req, res) => {
-  const { name, mail, ubicacion, iduser } = req.body;
-  const query = 'UPDATE usuarios SET Nombre = ?, Mail = ?, Ubicación = ? WHERE ID_usuario = ?';
-  connection.query(query, [name, mail, ubicacion, iduser], (error, result) => {
+  const { name, mail, ubicacion, contrasena, iduser } = req.body;
+  const query = 'UPDATE usuarios SET Nombre = ?, Mail = ?, Ubicación = ?, Contrasena = ? WHERE ID_usuario = ?';
+  connection.query(query, [name, mail, ubicacion, contrasena, iduser], (error, result) => {
     if (error) {
       console.error('Error al actualizar el registro en la tabla "usuarios": ', error);
       res.status(500).send('Error del servidor');
