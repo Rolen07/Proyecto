11bb1b9963dbd968af3e3a3460a850c674f70cce
Spoopypingas <arranzgomezjoseluis@gmail.com>
2024-05-23 20:04:13 +0200

    wthboi

--- routers/update.js
+++ routers/update.js
@@ -1,39 +1,33 @@
 const express = require('express');
-
-const actualizar = express.Router();  //Este objeto es que vamos a exportar
-
-const url = require('url');
+const actualizar = express.Router(); // Este objeto es que vamos a exportar
 const connection = require('../database.js');
 
-
-
-  //Página para presentar datos para actualizar los grupos
-  actualizar.get('/groups', (req, res)=> {
-    const parametro = url.parse(req.url, true);
-    const idg = parametro.query.idg;
-    const query = 'SELECT * FROM grupos WHERE ID_grupo="' + idg + '"';
-    connection.query(query, (error, results) => { 
-      if (error) {
-        console.error('Error al obtener los registros de la tabla "grupos": ', error);
-        res.status(500).send('Error del servidor');
-      } else {
-        res.render('updateGrupos', { groups: results });
-      }
-     });
-  })
-  
-  // Página de actualización de grupos
-  actualizar.post('/groups', (req, res) => {
-    const { name, desc, idg} = req.body;
-    const query = 'UPDATE grupos SET Nombre_grupo = ?, Descripcion = ? WHERE ID_grupo =?';
-    connection.query(query, [name, desc, idg], (error, result) => {
-      if (error) {
-        console.error('Error al insertar el nuevo registro en la tabla "grupos": ', error);
-        res.status(500).send('Error del servidor');
-      } else {
-        res.redirect('../list/groups');
-      }
-    });
+// Página para presentar datos para actualizar el perfil del usuario
+actualizar.get('/actdatos', (req, res) => {
+  const iduser = parametro.query.ID_usuario;
+  const query = 'SELECT * FROM usuarios WHERE `ID_usuario`="' + iduser + '"';
+  connection.query(query, [userID], (error, results) => {
+    if (error) {
+      console.error('Error al obtener los registros de la tabla "usuarios": ', error);
+      res.status(500).send('Error del servidor');
+    } else {
+      res.render('updatePerfil', { perfil: results[0], userID: userID });
+    }
   });
+});
 
-  module.exports = actualizar;
+// Página de actualización del perfil del usuario
+actualizar.post('/actdatos', (req, res) => {
+  const { name, mail, ubicacion, iduser } = req.body;
+  const query = 'UPDATE usuarios SET Nombre = ?, Mail = ?, Ubicación = ? WHERE ID_usuario = ?';
+  connection.query(query, [name, mail, ubicacion, iduser], (error, result) => {
+    if (error) {
+      console.error('Error al actualizar el registro en la tabla "usuarios": ', error);
+      res.status(500).send('Error del servidor');
+    } else {
+      res.redirect('/principal');
+    }
+  });
+});
+
+module.exports = actualizar;
