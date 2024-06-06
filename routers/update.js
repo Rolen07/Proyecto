fa3af46f4bbb5e30e35756e5d40f87f1c566029c
Makus22 <makus.651@gmail.com>
2024-06-06 11:31:10 +0200

    Update update.js

--- routers/update.js
+++ routers/update.js
@@ -2,20 +2,6 @@ const express = require('express');
 const actualizar = express.Router(); // Este objeto es el que vamos a exportar
 const connection = require('../database.js');
 
-// Página para presentar datos para actualizar el perfil del usuario
-actualizar.get('/actdatos', (req, res) => {
-  const iduser = req.query.ID_usuario;
-  const query = 'SELECT * FROM usuarios WHERE ID_usuario = ?';
-  connection.query(query, [iduser], (error, results) => {
-    if (error) {
-      console.error('Error al obtener los registros de la tabla "usuarios": ', error);
-      res.status(500).send('Error del servidor');
-    } else {
-      res.render('updatePerfil', { perfil: results[0], userID: iduser });
-    }
-  });
-});
-
 // Página de actualización del perfil del usuario
 actualizar.post('/actdatos', (req, res) => {
   const { name, mail, ubicacion, contrasena, iduser } = req.body;
@@ -30,7 +16,7 @@ actualizar.post('/actdatos', (req, res) => {
   });
 });
 
-// Página de actualización del perfil del usuario
+// Página de actualización de la membresia dentro de perfil 
 actualizar.post('/actmembresia', (req, res) => {
   const { userID } = req.body;
   const query = 'UPDATE usuarios SET Miembro = "SI" WHERE ID_usuario = ?';
